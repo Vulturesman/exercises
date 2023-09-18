675c151cb92c260b81a9faf18576a1c4bfa473e3
Jakub Kozler <kozlerjakub@gmail.com>
2023-09-18 15:40:03 +0200

    another exercises

--- day11/javascript-function/js/functions.js
+++ day11/javascript-function/js/functions.js
@@ -3,9 +3,14 @@ const setHeadline = () => {
     element.innerHTML = ('JavaScript functions')
 }
 
-const input = prompt('Write your input');
+// const input = prompt('Write your input');
 
-const changeOther = () => {
-    let head = document.getElementById('headline-other')
-    head.innerHTML = (input)
+// const changeOther = () => {
+//     let head = document.getElementById('headline-other')
+//     head.innerHTML = (input)
+// }
+
+const test = (id, text) => {
+    let checkElement = document.getElementById(id);
+    checkElement.innerHTML = text;
 }
