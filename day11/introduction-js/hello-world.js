5b66a54a2a301fd9da0a9c3a6e8c655a5a503953
Jakub Kozler <kozlerjakub@gmail.com>
2023-09-18 13:25:39 +0200

    adjustment day 11

--- day11/introduction-js/hello-world.js
+++ day11/introduction-js/hello-world.js
@@ -1,23 +1,48 @@
-alert('Hello from an external file')
+// alert('Hello from an external file')
 
-console.log('Hello, world!');
+// console.log('Hello, world!');
 
-console.log('Wrong arguments!');
+// console.log('Wrong arguments!');
 
-let numero = 3;
-console.log(typeof numero);
+// let numero = 3;
+// console.log(typeof numero);
 
-let stringos = "hi"
-console.log(typeof stringos);
+// let stringos = "hi"
+// console.log(typeof stringos);
 
-let boolos = false;
-console.log(typeof boolos);
+// let boolos = false;
+// console.log(typeof boolos);
 
-let nulkos = 0;
-console.log(typeof nulkos);
+// let nulkos = 0;
+// console.log(typeof nulkos);
 
-let und = '';
-console.log(typeof und);
+// let und = '';
+// console.log(typeof und);
 
-let arr = [1, 2, 3]
-console.log(typeof arr);
+// let arr = [1, 2, 3]
+// console.log(typeof arr);
+
+// let number1 = 0;
+// number1++;
+// console.log(number1);
+
+
+// let number2 = 0;
+// console.log(number2++);
+
+
+// let number3 = 0;
+// console.log(++number3);
+
+
+// let foo = 1;
+
+// foo += 2;
+
+// console.log(foo);
+ 
+// foo -= 2; // lowers the value by 2
+// foo /= 2; // halves the value
+// foo *= 2; // raises the value twice
+
+
