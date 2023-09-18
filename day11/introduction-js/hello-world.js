3bb4d319e2a52cf5b7f97528dd842ccc558de363
Jakub Kozler <kozlerjakub@gmail.com>
2023-09-18 13:01:27 +0200

    added new files from day 11

---
+++ day11/introduction-js/hello-world.js
@@ -0,0 +1,23 @@
+alert('Hello from an external file')
+
+console.log('Hello, world!');
+
+console.log('Wrong arguments!');
+
+let numero = 3;
+console.log(typeof numero);
+
+let stringos = "hi"
+console.log(typeof stringos);
+
+let boolos = false;
+console.log(typeof boolos);
+
+let nulkos = 0;
+console.log(typeof nulkos);
+
+let und = '';
+console.log(typeof und);
+
+let arr = [1, 2, 3]
+console.log(typeof arr);
