98df09689bd0d3ef97b6b408414c76a6b1d69828
Jakub Kozler <kozlerjakub@gmail.com>
2023-09-22 15:36:17 +0200

    afternoon part 2

--- day15/simple-grocery/script.js
+++ day15/simple-grocery/script.js
@@ -1,19 +1,52 @@
 const groceryList = [
-  "apples",
-  "bananas",
-  "milk",
-  "bread",
-  "pineapple",
-  "chicken breasts",
-  "iPhone15",
-  "spaghetti",
-  "oranges",
-  "wheels",
-  "tickets",
+  {
+    name: "apples",
+    amount: "4",
+  },
+  {
+    name: "bananas",
+    amount: "10",
+  },
+  {
+    name: "milk",
+    amount: "1",
+  },
+  {
+    name: "bread",
+    amount: "2",
+  },
+  {
+    name: "pineapple",
+    amount: "7",
+  },
+  {
+    name: "chicken breasts",
+    amount: "2",
+  },
+  {
+    name: "iPhone15",
+    amount: "1",
+  },
+  {
+    name: "spaghetti",
+    amount: "4",
+  },
+  {
+    name: "oranges",
+    amount: "4",
+  },
+  {
+    name: "wheels",
+    amount: "2",
+  },
+  {
+    name: "tickets",
+    amount: "10",
+  },
 ];
 
 const listDiv = document.querySelector(".list");
 
 groceryList.forEach((groceryItem) => {
-  listDiv.innerHTML += `<div class="item">${groceryItem}</div>`;
+  listDiv.innerHTML += `<div class="item">${groceryItem.name}, pieces: ${groceryItem.amount}</div>`;
 });
--- day15/train-station/script.js
+++ day15/train-station/script.js
@@ -1,21 +1,23 @@
-const departures = [
-  494, 499, 500, 517, 520, 523, 530, 556, 563, 576, 586, 613, 625,
-];
+// STEP 1
 
-const ul = document.createElement("ul");
-document.body.appendChild(ul);
+// const departures = [
+//   494, 499, 500, 517, 520, 523, 530, 556, 563, 576, 586, 613, 625,
+// ];
 
-for (let i = 0; i < departures.length; i++) {
-  const departureHours = Math.floor(departures[i] / 60);
-  const departureMinutes = departures[i] % 60;
+// const ul = document.createElement("ul");
+// document.body.appendChild(ul);
 
-  ul.innerHTML += `<li>${departureHours}:${departureMinutes}</li>`;
+// for (let i = 0; i < departures.length; i++) {
+//   const departureHours = Math.floor(departures[i] / 60);
+//   const departureMinutes = departures[i] % 60;
 
-  //   div solution
-  //   const div = document.createElement("div");
-  //   document.body.appendChild(div);
-  //   div.textContent = `${departureHours}:${departureMinutes}`;
-}
+//   ul.innerHTML += `<li>${departureHours}:${departureMinutes}</li>`;
+
+//   div solution
+//   const div = document.createElement("div");
+//   document.body.appendChild(div);
+//   div.textContent = `${departureHours}:${departureMinutes}`;
+// }
 
 // -----------
 /* solution CBP  difference in UL (alredy created in HTML) $ forEach method
@@ -29,3 +31,145 @@ departures.forEach((time) => {
 
   listElm.innerHTML += `<li>${hours}:${minutes}</li>`;
 }); */
+
+// STEP 2
+
+// creating elements via createElement
+
+// const table = document.createElement("table");
+// document.body.appendChild(table);
+// const tHead = document.createElement("thead");
+// table.appendChild(tHead);
+// const th = document.createElement("th");
+// tHead.appendChild(th);
+// th.innerText = "Time";
+// const tBody = document.createElement("tbody");
+// table.appendChild(tBody);
+
+// for (let i = 0; i < departures.length; i++) {
+//   const departureHours = Math.floor(departures[i] / 60);
+//   const departureMinutes = departures[i] % 60;
+
+//   tBody.innerHTML += `<tr><td>${departureHours}:${departureMinutes}</td></tr>`;
+// }
+
+const departures2 = [
+  {
+    time: { hrs: 8, mins: 14 },
+    train: "Acela Express",
+    no: 2153,
+    to: "Washington, DC",
+    status: "",
+    track: 2,
+  },
+  {
+    time: { hrs: 8, mins: 19 },
+    train: "New Haven Line",
+    no: 1541,
+    to: "Grand Central Terminal",
+    status: "On Time",
+    track: 14,
+  },
+  {
+    time: { hrs: 8, mins: 20 },
+    train: "Shore Line East",
+    no: 1606,
+    to: "Old Saybrook",
+    status: "On Time",
+    track: 12,
+  },
+  {
+    time: { hrs: 8, mins: 37 },
+    train: "N.E. Regional",
+    no: 190,
+    to: "Boston South Station",
+    status: "On Time",
+    track: 1,
+  },
+  {
+    time: { hrs: 8, mins: 40 },
+    train: "Shuttle",
+    no: 490,
+    to: "Springfield",
+    status: "On Time",
+    track: 3,
+  },
+  {
+    time: { hrs: 8, mins: 43 },
+    train: "N.E. Regional",
+    no: 96,
+    to: "Newport News",
+    status: "On Time",
+    track: 2,
+  },
+  {
+    time: { hrs: 8, mins: 50 },
+    train: "New Haven Line",
+    no: 1545,
+    to: "Grand Central Terminal",
+    status: "On Time",
+    track: 8,
+  },
+  {
+    time: { hrs: 9, mins: 16 },
+    train: "Shore Line East",
+    no: 1610,
+    to: "Old Saybrook",
+    status: "On Time",
+    track: 10,
+  },
+  {
+    time: { hrs: 9, mins: 23 },
+    train: "New Haven Line",
+    no: 1549,
+    to: "Grand Central Terminal",
+    status: "On Time",
+    track: 14,
+  },
+  {
+    time: { hrs: 9, mins: 36 },
+    train: "Acela Express",
+    no: 2150,
+    to: "Boston-South Sta.",
+    status: "",
+    track: 1,
+  },
+  {
+    time: { hrs: 9, mins: 46 },
+    train: "New Haven Line",
+    no: 1551,
+    to: "Grand Central Terminal",
+    status: "On Time",
+    track: 8,
+  },
+  {
+    time: { hrs: 10, mins: 13 },
+    train: "N.E. Regional",
+    no: 170,
+    to: "Boston South Station",
+    status: "On Time",
+    track: 2,
+  },
+  {
+    time: { hrs: 10, mins: 25 },
+    train: "New Haven Line",
+    no: 1553,
+    to: "Grand Central Terminal",
+    status: "On Time",
+    track: 14,
+  },
+];
+
+const tbody = document.querySelector(".board__table tbody");
+departures2.forEach((row) => {
+  tbody.innerHTML += `
+    <tr>
+      <td class="board__time">${row.time.hrs}:${row.time.mins}</td>
+      <td class="board__train">${row.train}</td>
+      <td class="board__no">${row.no}</td>
+      <td class="board__to">${row.to}</td>
+      <td class="board__status">${row.status}</td>
+      <td class="board__track">${row.track}</td>
+    </tr>
+  `;
+});
