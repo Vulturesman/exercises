a8ea7d775e54c81564c447449224e1a5e7d0ec4c
Jakub Kozler <kozlerjakub@gmail.com>
2023-09-26 19:44:44 +0200

    final exercises

--- day17/afternoon-fetch/src/js/index.js
+++ day17/afternoon-fetch/src/js/index.js
@@ -1,33 +1,41 @@
-document.addEventListener("DOMContentLoaded", async () => {
-  const response = await fetch(
-    "https://classes.codingbootcamp.cz/assets/classes/602/guardian.php"
-  );
+/* Idea 1: Cat facts
+Use the API endpoints at catfact.ninja
+Create a page with a nice cat image and a big button saying "Give me another cat fact".
+When the user clicks the button, a random fact is loaded from the API and shown.
+*/
+
+const loadData = async () => {
+  const response = await fetch("https://catfact.ninja/fact");
   const data = await response.json();
-  const nav = document.querySelector(".navigation");
 
-  data.data.forEach((category) => {
-    const a = document.createElement("a");
-    a.textContent = category;
-    nav.appendChild(a);
+  const catsHTML = document.querySelector("p.cats_p");
+  catsHTML.innerHTML = "";
+  const result = data.fact;
 
-    a.addEventListener("click", () => {
-      loadArticles(category);
-    });
-  });
+  catsHTML.innerHTML = result;
+};
+
+const button = document.querySelector("#cat_btn");
+button.addEventListener("click", () => {
+  loadData();
 });
 
-const loadArticles = async (category) => {
-  const response = await fetch(
-    `https://classes.codingbootcamp.cz/assets/classes/602/guardian.php?cat=${category}`
-  );
+/* ------------------------------------
+Idea 2: Cryptocurrency prices
+Use the API endpoint at https://api2.binance.com/api/v3/ticker/24hr
+Create a page showing the current price of your favorite cryptocurrency. Attach the loading of the data to a interval so that it refreshes every couple of seconds. */
+
+const loadCrypto = async (change) => {
+  const response = await fetch(" https://api2.binance.com/api/v3/ticker/24hr");
   const data = await response.json();
 
-  const articleList = document.getElementById("articleList");
-  articleList.innerHTML = "";
+  const cryptoHTML = document.querySelector(".crypto");
+  const parCrypto = document.createElement("p");
+  cryptoHTML.appendChild(parCrypto);
 
-  data.data.channel.item.forEach((item) => {
-    const listItem = document.createElement("li");
-    listItem.innerHTML = `${item.title} + link <a href="${item.link}"> HERE.`;
-    articleList.appendChild(listItem);
+  const findElement = data.find((symbol) => {
+    parCrypto.innerHTML === "change";
   });
 };
+
+setInterval(loadCrypto("SYSBTC"), 10000);
