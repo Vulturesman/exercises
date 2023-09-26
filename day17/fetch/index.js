b6f7e2e4acb5ac2591d2a7eb7489329df4bd2e08
Jakub Kozler <kozlerjakub@gmail.com>
2023-09-26 14:23:25 +0200

    afternoon exercises start

--- day17/fetch/index.js
+++ day17/fetch/index.js
@@ -41,3 +41,47 @@ loadData2();
 console.log("B");
 
 /* it will log A C B D -> loadData2 is just defined, then it goes to log A, then calling function, it takes C, then it is waiting for promise from fetch, it ends/skips the function and goes to the first line after calling of function, it logs B, and once the promise is dealt, it goes back to the function and logs D */
+
+// -------------------------------------------
+/*
+Run a fetch AJAX GET request call onto this url: https://classes.codingbootcamp.cz/assets/classes/602/guardian.php using async - await and place the response object into a constant.
+const response = await fetch('https://classes.codingbootcamp.cz/assets/classes/602/guardian.php');
+Log the response into the console.
+Then get the data from the response with another await:
+const data = await response.json();
+Now log the response data into the console.
+*/
+
+const loadData10 = async () => {
+  const response = await fetch(
+    "https://classes.codingbootcamp.cz/assets/classes/602/guardian.php"
+  );
+  const data = await response.json();
+};
+
+loadData10();
+
+// Use fetch to retrieve data from the URL https://classes.codingbootcamp.cz/assets/classes/602/guardian.php?cat=science
+// Log the response data into the console and try to make sense of its structure.
+// Then use JavaScript and HTML elements creation to display a list of current science articles from the Guardian!
+
+const loadData20 = async () => {
+  const response = await fetch(
+    "https://classes.codingbootcamp.cz/assets/classes/602/guardian.php?cat=science"
+  );
+  const data = await response.json();
+
+  return data;
+};
+
+loadData20().then((data) => {
+  const ul = document.createElement("ul");
+  document.body.appendChild(ul);
+
+  data.data.channel.item.forEach((i) => {
+    const li = document.createElement("li");
+    ul.appendChild(li);
+
+    li.innerHTML = `${i.title} with link<a href="${i.link}">HERE</a>`;
+  });
+});
