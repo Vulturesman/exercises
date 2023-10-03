ee8e291d59ca3e9f6ac872bc38ff5205d24a056f
Jakub Kozler <kozlerjakub@gmail.com>
2023-10-03 08:37:06 +0200

    afternoon ex

--- day21/encyklopedia/src/App.jsx
+++ day21/encyklopedia/src/App.jsx
@@ -25,7 +25,10 @@ const App = () => {
         <Term title="Blabla term3" definition="some definition3"></Term>
         </div>
         <div className='question'>
-          <Question title='Question' text='This is the best answer'></Question>
+          <Question title='Question?' text='This is the best answer I can get'></Question>
+           <Question title="What’s the big deal with React.js?" text="What are some facts about why React is technically superior" />
+            <Question title="Is React.js worth it?" text="Let us know" />
+            <Question title="Should I learn React.js or Vue.js?" text="Basically is it worth it if I learn Vue.js first and then React.js" />
         </div>
 
         <div className='quote'>
--- day21/encyklopedia/src/Question.jsx
+++ day21/encyklopedia/src/Question.jsx
@@ -1,9 +1,14 @@
+import { Answer } from "./Answer"
+
 export const Question = ({title, text}) => {
 return (
-<>
-<h3>{title}</h3>
-<p><strong>{text}</strong></p>
-</>
+
+<div className="question">
+    <h3 className="question__title">{title}</h3>
+    <p className="question__text"><strong>{text}</strong></p>
+    <Answer answer="Answer 1"/>
+    <Answer answer="Answer 2"/>
+</div>
 
 )
 
