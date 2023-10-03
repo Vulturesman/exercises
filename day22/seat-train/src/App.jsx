ec0afce2b2b3e5ca0171fff906381e81d4b81237
Jakub Kozler <kozlerjakub@gmail.com>
2023-10-03 20:02:19 +0200

    finalized

---
+++ day22/seat-train/src/App.jsx
@@ -0,0 +1,32 @@
+import React, {useState} from "react";
+
+import './index.css';
+import {Seat} from "./Seat";
+
+
+function App() {
+    const numberOfSeats = 48;
+
+    return (
+        <div className="App">
+            <div className="carriage">
+                {
+                    ( new Array( numberOfSeats / 4 ) ).fill( null ).map( ( n, row ) => (
+                        <div key={row} className="carriage-row">
+                            <Seat number={row * 4 + 1}/>
+                            <Seat number={row * 4 + 2}/>
+
+                            <div className="aisle-spacer"></div>
+
+                            <Seat number={row * 4 + 3}/>
+                            <Seat number={row * 4 + 4}/>
+                        </div>
+                    ) )
+                }
+
+            </div>
+        </div>
+    );
+}
+
+export default App
