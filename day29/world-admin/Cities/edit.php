73bd85bd5dfca5462aca01948c5b2bd34058f06f
Jakub Kozler <kozlerjakub@gmail.com>
2023-10-12 16:47:43 +0200

    afternoon ex

--- day29/world-admin/Cities/edit.php
+++ day29/world-admin/Cities/edit.php
@@ -8,6 +8,8 @@ if (isset($_GET['id'])) {
     $formType = "Edit";
 }
 
+$successMessages = session()->get('success_messages');
+var_dump($successMessages);
 
 ?>
 
@@ -15,21 +17,33 @@ if (isset($_GET['id'])) {
 <html lang="en">
 
 <head>
+    <style>
+        .success-message {
+            color: red;
+            background-color: lightgreen;
+        }
+
+        .error-message {
+            background-color: red;
+            color: white;
+        }
+    </style>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $formType ?> city</title>
 </head>
 
 <body>
+    <?php include './alerts.php' ?>
     <h1><?= $formType ?> city:</h1>
     <form method="post" action="store.php">
         <input id="id" name="id" type="hidden" value="<?= $city->id ?>" />
         <label for="name">Name:</label>
-        <input id="name" name="name" type="text" value="<?= $city->name ?>" />
+        <input id="name" name="name" type="text" value="<?= htmlspecialchars((string) old('name', $city->name)) ?>" />
         <label for="district">District:</label>
-        <input id="district" name="district" type="text" value="<?= $city->district ?>" />
+        <input id="district" name="district" type="text" value="<?= htmlspecialchars((string)old('district', $city->district)) ?>" />
         <label for="district">Population:</label>
-        <input id="population" name="population" type="number" min="0" value="<?= $city->population ?>" />
+        <input id="population" name="population" type="number" min="0" value="<?= htmlspecialchars(old('population', $city->population)) ?>" />
         <!-- TODO: GET COUNTRIES FROM DB -->
         <!-- <select>
             <?php foreach ($countries as $country) : ?>
--- day29/world-admin/Cities/store.php
+++ day29/world-admin/Cities/store.php
@@ -2,16 +2,46 @@
 require_once './bootstrap.php';
 
 $data = $_POST;
-$city = new City();
-if ($haveId = isset($data['id'])) {
-    $city->findById($data['id']);
+$id = $data['id'] ?? null;
+
+$errors = [];
+if (!isset($data['name']) || is_numeric($data['name']))
+{
+    $errors[] = 'The name field is required and cannot be a number!';
 }
+if (!isset($data['district']) || is_numeric($data['district']))
+{
+    $errors[] = 'The district field is required and cannot be a number!';
+}
+if (!isset($data['population']) || !is_numeric($data['population']))
+{
+    $errors[] = 'The population field is required!';
+}
+
+if(!empty($errors))
+{
+    session()->flash('errors', $errors);
+    session()->flashRequest();
+    header("Location: edit.php?id=$id");
+    exit();
+}
+
+$city = new City();
+if ($id)
+{
+    $city->findById($data['id']);
+    $action = 'edited';
+}
+
+
+$action = 'inserted';
+
 
 $city->name = $data['name'];
 $city->district = $data['district'];
 $city->population = $data['population'];
 $city->save();
 
-session()->flash('success', 'The city was inserted');
+session()->flash('success_message', "The city was $action");
 
 header("Location: edit.php?id=$city->id");
